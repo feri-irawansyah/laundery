c55d7e902f6050f4a58b33cd698ad96619b93e35
RizqyDS <rizqysyahputraa@gmail.com>
2025-06-03 21:57:54 +0700

    add frontend project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-center text-4xl font-bold text-blue-500 uppercase'>Hello World</h1>
+    </>
+  )
+}
+
+export default App
