6e55cce44f043ea9250363083484116a6be04e19
RizqyDS <rizqysyahputraa@gmail.com>
2025-06-08 20:46:03 +0700

    Create Navbar and Components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import './App.css'
-import Navbar from './components/navbar'
+import { Route, Routes } from "react-router";
+import "./App.css";
+import Navbar from "./components/navbar";
+import About from "./components/About";
+import Home from "./components/Home";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 
 function App() {
-
   return (
     <>
-      <Navbar />
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import { BiLogoReact } from "react-icons/bi";
+import { Link } from "react-router";
 
 const Navbar = () => {
   return (
-    <div className='text-4xl text-blue-500 font-semibold text-center'>Navbar</div>
-  )
-}
+    <nav
+      className="bg-slate-800 shadow-lg flex items-center 
+      justify-around py-3 px-32 fixed top-0 left-0 w-full">
+      <Link to="/">
+        <span
+          className="font-semibold text-lg flex items-center 
+        gap-3 text-blue-400"
+        >
+          <BiLogoReact className="text-6xl" />
+          <span className="font-semibold text-2xl">React Router</span>
+        </span>
+      </Link>
 
-export default Navbar
+      <div className="flex items-center gap-5 text-black">
+      <Link
+        to="/"
+        className="py-1 px-3 text-lg font-light text-white 
+        hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300"
+      >
+        Home
+      </Link>
+
+      <Link
+        to="/about"
+        className="py-1 px-3 text-lg font-light text-white 
+        hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300"
+      >
+        About
+      </Link>
+
+      <Link
+        to="/services"
+        className="py-1 px-3 text-lg font-light text-white 
+        hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300"
+      >
+        Services
+      </Link>
+
+      <Link
+        to="/contact"
+        className="py-1 px-3 text-lg font-light text-white 
+        hover:text-sky-300 rounded-2xl hover:bg-slate-700 transition duration-300"
+      >
+        Contact
+      </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
